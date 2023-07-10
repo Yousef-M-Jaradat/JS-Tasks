f3747f9d0761ff2ea8d028817d969baeec349f5a
Yousef-Jaradat <135589126+Yousef-Jaradat@users.noreply.github.com>
2023-07-10 23:52:46 +0300

    upload

---
+++ Task2(7-10-2023)/exersices1.js
@@ -0,0 +1,63 @@
+/* 1. Write a JavaScript program that accept two integers and
+ * display the larger.Go to the editor */
+/******* Start Your Code *********/
+console.log("******************* Task1 Question1******************");
+
+let x1 = -5;
+let y1 = 10;
+if( x1 > y1){
+    console.log(x1);
+}else {
+    console.log(y1);
+}
+
+
+ /******* End Your Code ********* */
+
+/* 2. Write a JavaScript conditional statement to find the sign
+ of product of three numbers. Display an alert box with the 
+ specified sign. Go to the editor 
+Sample numbers : 3, -7, 2 
+Output : The sign is - */
+/******* Start Your Code *********/
+console.log("******************* Task1 Question2******************");
+
+let arr2=[3, -7, 2];
+if (arr2[0] < 0){
+    console.log(``);
+}
+
+ /******* End Your Code ********* */
+
+/* 3. Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor 
+Sample numbers : 0, -1, 4 
+Output : 4, 0, -1 */
+/******* Start Your Code *********/
+console.log("******************* Task1 Question1******************");
+
+let x3 = 0;
+let y3 = -1;
+let z3 = 4;
+
+if( x3 > y3 && x3 > z3 && y3 > z3){
+    console.log(x3,y3,z3);
+}
+ /******* End Your Code ********* */
+
+
+/*4. Write a JavaScript conditional statement to find the largest of five numbers. 
+Display an alert box to show the result. Go to the editor 
+Sample numbers : -5, -2, -6, 0, -1 
+Output : 0 */
+
+/******* Start Your Code *********/
+  
+ /******* End Your Code ********* */
+
+ /* 5.Write the if statement to display "Hello World" if x is greater than y, otherwise alert "Goodbye".*/
+/******* Start Your Code *********/
+  
+ /******* End Your Code ********* */
+
+
+
---
+++ Task2(7-10-2023)/exersices3.js
@@ -0,0 +1,25 @@
+// 1. Write a JS code to print numbers from 1 to 10
+
+
+/*2. Write a JS code to print Even numbers in arr :
+   var arr = [13,23,12,45,22,48,66,100]
+*/
+
+/* 
+3. Write a JS code to print a pattern using for loop
+
+   1 
+   1 2 
+   1 2 3 
+   1 2 3 4 
+   1 2 3 4 5 
+   1 2 3 4 5 6 
+   1 2 3 4 5 6 7 
+   1 2 3 4 5 6 7 8 
+
+*/
+ /* 
+ Check if a string contains the letter “y”. Print “yes” if it does and “no” if it does not.
+let x = "don’t know why"
+
+*/
