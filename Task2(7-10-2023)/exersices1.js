2049278a5db427f2301a0e663dcb426e498b6d36
Yousef-Jaradat <135589126+Yousef-Jaradat@users.noreply.github.com>
2023-07-11 00:11:52 +0300

    upload

--- Task2(7-10-2023)/exersices1.js
+++ Task2(7-10-2023)/exersices1.js
@@ -15,8 +15,8 @@ if( x1 > y1){
  /******* End Your Code ********* */
 
 /* 2. Write a JavaScript conditional statement to find the sign
- of product of three numbers. Display an alert box with the 
- specified sign. Go to the editor 
+of product of three numbers. Display an alert box with the 
+specified sign. Go to the editor 
 Sample numbers : 3, -7, 2 
 Output : The sign is - */
 /******* Start Your Code *********/
@@ -33,7 +33,7 @@ if (arr2[0] < 0){
 Sample numbers : 0, -1, 4 
 Output : 4, 0, -1 */
 /******* Start Your Code *********/
-console.log("******************* Task1 Question1******************");
+console.log("******************* Task1 Question3******************");
 
 let x3 = 0;
 let y3 = -1;
@@ -61,7 +61,7 @@ Sample numbers : -5, -2, -6, 0, -1
 Output : 0 */
 
 /******* Start Your Code *********/
-console.log("******************* Task1 Question1******************");
+console.log("******************* Task1 Question4******************");
 
 let a4 = -5 ,b4 = -2 ,c4 = -6 ,d4 = 0 ,e4 =-1 ;
 
@@ -81,7 +81,17 @@ if(a4 > b4 && a4 > c4 && a4 > d4 && a4 > e4){
 
  /* 5.Write the if statement to display "Hello World" if x is greater than y, otherwise alert "Goodbye".*/
 /******* Start Your Code *********/
-  
+console.log("******************* Task1 Question5******************");
+
+let x,y;
+x5 = 55;
+y5 = -55
+if(x5 > y5){
+    console.log("Hello World");
+}else{
+    console.log("Goodbye");
+}
+
  /******* End Your Code ********* */
 
 
--- Task2(7-10-2023)/exersices2.js
+++ Task2(7-10-2023)/exersices2.js
@@ -6,3 +6,35 @@ equal or between 70 and 79 ===> C
 equal or between 80 and 89 ===> B
 equal or between 90 and 100 ===> A
 */
+console.log("******************* Task2 Question1******************");
+
+let rate,x;
+rate = 99;
+if (rate < 50 ){
+    x = 0;
+}else if (rate >= 50 && rate <= 59){
+    x = 1;
+}else if (rate >= 60 && rate <= 69){
+    x = 2 ;
+}else if (rate >= 70 && rate <= 79){
+    x = 3;
+}else if (rate >= 80 && rate <= 89){
+    x = 4;
+}else if (rate >= 90 && rate <= 100){
+    x = 5;
+}
+
+switch(rate){
+    case 0 : console.log("Fail");
+    break;
+    case 1 : console.log("E");
+    break;
+    case 2 : console.log("D");
+    break;
+    case 3 : console.log("C");
+    break;
+    case 4 : console.log("B");
+    break;
+    case 5 : console.log("A");
+    break;
+}
