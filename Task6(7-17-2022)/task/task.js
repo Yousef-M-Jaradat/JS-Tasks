836a6e4422f76cf73a2c8c87f085c9d34ba69863
Yousef-Jaradat <135589126+Yousef-Jaradat@users.noreply.github.com>
2023-07-18 00:16:24 +0300

    upload

--- Task6(7-17-2022)/task/task.js
+++ Task6(7-17-2022)/task/task.js
@@ -1,12 +1,27 @@
-const x = new XMLHttpRequest();
+let btn = document.getElementById("btn");
+btn.onclick = function(){
+    const x = new XMLHttpRequest();
+x.open('GET',"task.json");//
 
-x.open('GET','task.json');
+let table = document.querySelector('table');
 
-x.onload = function() {
-    const y = JSON.parse(this.responseText);
-    console.log(y);
-
-    
+x.onload = function(){
+    let tableobj = JSON.parse(x.responseText);//ملف الجيسون عبارة عن txt
+    for (let i=0; i<tableobj.length; i++) {
+        let trtable = document.createElement('tr');
+        table.appendChild(trtable);
+        for (const key in tableobj[i]) {
+            if (key === "image"){
+                let tdtable = document.createElement("td");
+                tdtable.innerHTML = `<img style="width: 100px; height: 100px;" src="${tableobj[i][`${key}`]}">`;  
+                trtable.appendChild(tdtable);              
+            }else{
+            let tdtable = document.createElement("td");
+            tdtable.innerHTML = tableobj[i][`${key}`];
+            trtable.appendChild(tdtable);
+        }
+    }
+        }
 }
-
-x.send()
+x.send();//تبعثه لملف الhtml
+}
